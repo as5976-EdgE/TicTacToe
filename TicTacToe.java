69687aff803100da8446ef308f48ce8e5b445609
as5976-EdgE <as5976@srmist.edu.in>
2026-04-30 19:20:17 +0530

    Complete UC7: Implement random valid move logic for computer

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,43 @@
+import java.util.Random;
 
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+    
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
-        placeMove(0, 0, 'X');
-        System.out.println(board[0][0]);
+        computerMove();
     }
-    static void placeMove(int row, int col, char symbol) {
-		
-        board[row][col] = symbol;
+
+    static void computerMove() {
+        Random random = new Random();
+        boolean validMoveFound = false;
+
+        
+        while (!validMoveFound) {
+        
+            int randomSlot = random.nextInt(9) + 1; 
+
+            
+            int row = getRowFromSlot(randomSlot);
+            int col = getColFromSlot(randomSlot);
+
+            if (isValidMove(row, col)) {
+
+                placeMove(row, col, computerSymbol);
+                System.out.println("Computer placed '" + computerSymbol + "' at slot " + randomSlot);
+                validMoveFound = true; 
+            }
+        }
     }
+
+    static int getRowFromSlot(int slot) { return (slot - 1) / 3; }
+    static int getColFromSlot(int slot) { return (slot - 1) % 3; }
+    static boolean isValidMove(int row, int col) { return board[row][col] == '-'; }
+    static void placeMove(int row, int col, char symbol) { board[row][col] = symbol; }
 }
